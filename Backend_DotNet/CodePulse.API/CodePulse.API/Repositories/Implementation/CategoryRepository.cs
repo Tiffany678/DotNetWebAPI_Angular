10b4facaaba1565ff68071b7ea8fc43119dd5a38
Tiffany678 <52Tiffany.yang@gmail.com>
2024-02-28 08:19:00 -0500

    complete CRUD on Category

--- Backend_DotNet/CodePulse.API/CodePulse.API/Repositories/Implementation/CategoryRepository.cs
+++ Backend_DotNet/CodePulse.API/CodePulse.API/Repositories/Implementation/CategoryRepository.cs
@@ -1,6 +1,5 @@
 ﻿using CodePulse.API.Data;
 using CodePulse.API.Models.Domain;
-using CodePulse.API.Models.DTO;
 using CodePulse.API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,10 +8,12 @@ namespace CodePulse.API.Repositories.Implementation
     public class CategoryRepository : ICategoryRepository
     {
         private readonly ApplicationDbContext dbContext;
+
         public CategoryRepository(ApplicationDbContext dbContext)
         {
             this.dbContext = dbContext;
         }
+
         public async Task<Category> CreateAsync(Category category)
         {
             await dbContext.Categories.AddAsync(category);
@@ -21,11 +22,42 @@ namespace CodePulse.API.Repositories.Implementation
             return category;
         }
 
+        public async Task<Category?> DeleteAsync(Guid id)
+        {
+            var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingCategory is null)
+            {
+                return null;
+            }
+
+            dbContext.Categories.Remove(existingCategory);
+            await dbContext.SaveChangesAsync();
+            return existingCategory;
+        }
+
         public async Task<IEnumerable<Category>> GetAllAsync()
         {
             return await dbContext.Categories.ToListAsync();
+        }
 
-     
+        public async Task<Category?> GetById(Guid id)
+        {
+            return await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Category?> UpdateAsync(Category category)
+        {
+            var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == category.Id);
+
+            if (existingCategory != null)
+            {
+                dbContext.Entry(existingCategory).CurrentValues.SetValues(category);
+                await dbContext.SaveChangesAsync();
+                return category;
+            }
+
+            return null;
         }
     }
 }
--- Front-end_Angular/codepulse/src/app/features/category/services/category.service.ts
+++ Front-end_Angular/codepulse/src/app/features/category/services/category.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +15,27 @@ export class CategoryService {
 
   constructor(private http: HttpClient) { }
 
-  addCategory(model: AddCategoryRequest): Observable<void>{
-    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model);
-  }
-  getAllCategories(): Observable<Category[]>{
+  getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
   }
+
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
+
+
+  addCategory(model: AddCategoryRequest): Observable<void> {
+   
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
+  }
+
+  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest) : Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, updateCategoryRequest);
+  }
+
+  deleteCategory(id: string) : Observable<Category> {
+    return this.http.delete<Category>(`${environment.apiBaseUrl}/api/categories/${id}`)
+  }
+ 
+
 }
